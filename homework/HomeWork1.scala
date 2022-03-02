c48872352c89bcfadd1be375b35e2ce73d4ef0ca
yuriprym <samuel.young.103@gmail.com>
2022-03-01 21:31:43 -0500

    homework1 using grep for problem 5

--- homework/HomeWork1.scala
+++ homework/HomeWork1.scala
@@ -1,5 +1,11 @@
 import scala.io.AnsiColor._
 import java.util.Scanner
+import scala.util.matching.Regex
+
+//Saberina Young 
+//
+//
+//:
 object HomeWork1{
   def jsonLike(): Any ={
     println(
@@ -13,23 +19,43 @@ object HomeWork1{
      println(s"${RESET}${BOLD}${UNDERLINED}$name${RESET} , ${RESET}${BOLD}$age${RESET}")
   }
   def httpIndexer(): Unit = {
-    val http = "http://allaboutscala"
-    val ch = http.toCharArray()
-    println(ch(8))
-
+    val http = "http://allaboutscala".substring(8,9)
+    println(http)
+  }
+  def fullNameAndFavoriteMovie(wholeName:String, movie:String) : Unit = 
+  {
+    val name : Regex = "[A-Z]*[a-z]".r
+    for (patternMatch <- name.findAllMatchIn(wholeName)) 
+      printf("%s%n%s%n",patternMatch.group(1),patternMatch.group(2))
+    println(movie)
   }
   def main(args: Array[String]): Unit = {
     jsonLike()
     
     val sc = new Scanner(System.in)
-    
+    val kyd = new Scanner(System.in)
+    println("please enter your first name")
     val  name = sc.nextLine()
+    println("please enter your age")
     val  age  = sc.nextInt()
 
     nameAge(name,age)
     httpIndexer()
-    
+    println("please include enter your surname and family-name ")
+   
+    val surnameAndFamilyname= "Saberina Young"
 
+    var number = 7
+    println("What is your favorite Movie!")
+    val title = "Akria"
+
+
+    
+    fullNameAndFavoriteMovie(surnameAndFamilyname,title)
+
+
+    kyd.close()
+    sc.close()
 
 
   }
